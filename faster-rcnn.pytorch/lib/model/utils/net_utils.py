407d692890d90d5bcb9c6571d6a4b87306d977cc
malarsaravanan1991 <anbumalar.saravanan@gmail.com>
2020-04-10 17:52:29 -0400

    modified for 30 classes

--- faster-rcnn.pytorch/lib/model/utils/net_utils.py
+++ faster-rcnn.pytorch/lib/model/utils/net_utils.py
@@ -9,6 +9,30 @@ import cv2
 import pdb
 import random
 
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
+
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
